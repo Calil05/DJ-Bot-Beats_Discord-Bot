fcd8f43b17775d73c7771117ddb66d416e7a9470
Gabriel Calil <gabrielbcalil@gmail.com>
2024-01-10 16:16:00 -0300

    fix: youtube connection fixex

--- music.py
+++ music.py
@@ -2,8 +2,9 @@ from pytube import Search
 from pytube import YouTube
 import os
 import shutil    
+from key import music_path
 
-music_folder = 'D:\_Projetos Maiores\DJ Bot\DJ-Bot-Beats_Discord-Bot\music'
+music_folder = music_path
 
 def get_link(name):
     search_music = Search(f"{name}")
@@ -20,10 +21,10 @@ def download_video(name):
     video_ids = [video.video_id for video in yt_id]
 
     if (name.startswith("https://")):
-        print("Processing URL:", name)
+        print("Processando URL:", name)
         yt = YouTube(name)
     else:
-        print("Processing query:", name)
+        print("Processando query:", name)
         video_id = video_ids[0]
         base_url = f"https://www.youtube.com/watch?v={video_id}"
         yt = YouTube(base_url)
@@ -35,11 +36,11 @@ def download_video(name):
         audio_stream = yt.streams.filter(only_audio=True, file_extension="mp4").first()
         if audio_stream:
             audio_stream.download(output_path=output_path)
-            print(f"Downloaded: {yt.title}")
+            print("Baixado: {}".format(yt.title))
         else:
-            print(f"No available audio streams for: {yt.title}")
-    except Exception as e:
-        print(f"Error downloading video: {e}")
+            print("Sem fontes de audio: {}".format(yt.title))
+    except Exception as err:
+        print("Erro no download: {}".format(err))
 
 def find_music():
     return (os.listdir("music")[0])
